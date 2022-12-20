5b5c2d10224c12ad95854b3111d3db820192a3cb
kengoon <kengoon19@gmail.com>
2022-12-20 22:56:32 +0100

    initial commit

---
+++ sjgoogleplaybilling/src/main/java/com/sj/sjgoogleplaybilling/SJGooglePlayBilling.java
@@ -0,0 +1,228 @@
+package com.sj.sjgoogleplaybilling;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.android.billingclient.api.AcknowledgePurchaseParams;
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener;
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.ConsumeParams;
+import com.android.billingclient.api.ConsumeResponseListener;
+import com.android.billingclient.api.ProductDetails;
+import com.android.billingclient.api.ProductDetailsResponseListener;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchaseHistoryResponseListener;
+import com.android.billingclient.api.PurchasesResponseListener;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.QueryProductDetailsParams;
+import com.android.billingclient.api.QueryPurchaseHistoryParams;
+import com.android.billingclient.api.QueryPurchasesParams;
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.collect.ImmutableList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class SJGooglePlayBilling {
+    public BillingClient billingClient;
+    public BillingFlowParams billingFlowParams;
+    public final HashMap<String, ProductDetails> mProductDetails = new HashMap<>();
+    public PurchasesUpdatedListener purchasesUpdatedListener;
+    public ConsumeResponseListener consumeResponseListener;
+    public AcknowledgePurchaseResponseListener acknowledgePurchaseResponseListener;
+    public PurchaseHistoryResponseListener purchaseHistoryResponseListener;
+    public PurchasesResponseListener purchasesResponseListener;
+
+    public void makePayment(Context context, Activity activity, String feature, String productType, String productId) {
+        initializeBillingClient(context, productType);
+        startBillingClientConnection(productType, productId);
+        prepareBillingFlow(productId);
+
+        if (billingClient.isFeatureSupported(feature).getResponseCode() == BillingClient.BillingResponseCode.OK) {
+            // Launch the billing flow
+            BillingResult billingResult = billingClient.launchBillingFlow(activity, billingFlowParams);
+        }
+    }
+
+    private void prepareBillingFlow(String productId) {
+        final ProductDetails productDetails = mProductDetails.get(productId);
+        final List<ProductDetails.SubscriptionOfferDetails> subscriptionOfferDetails =
+                Objects.requireNonNull(productDetails).getSubscriptionOfferDetails();
+
+        ImmutableList<BillingFlowParams.ProductDetailsParams> productDetailsParamsList =
+                ImmutableList.of(
+                        BillingFlowParams.ProductDetailsParams.newBuilder()
+                                // retrieve a value for "productDetails" by calling queryProductDetailsAsync()
+                                .setProductDetails(Objects.requireNonNull(productDetails))
+                                // to get an offer token, call ProductDetails.getSubscriptionOfferDetails()
+                                // for a list of offers that are available to the user
+                                //.setOfferToken(Objects.requireNonNull(subscriptionOfferDetails).get(0).getOfferToken())
+                                .build()
+                );
+
+        billingFlowParams = BillingFlowParams.newBuilder()
+                .setProductDetailsParamsList(productDetailsParamsList)
+                .build();
+
+    }
+
+    private void startBillingClientConnection(String productType, String productId) {
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+                if (billingResult.getResponseCode() ==  BillingClient.BillingResponseCode.OK) {
+                    QueryProductDetailsParams queryProductDetailsParams =
+                            QueryProductDetailsParams.newBuilder().setProductList(
+                                    ImmutableList.of(
+                                            QueryProductDetailsParams.Product.newBuilder()
+                                                    .setProductId(productId)
+                                                    .setProductType(
+                                                            productType.equals("subs") ?
+                                                                    BillingClient.ProductType.SUBS :
+                                                                    BillingClient.ProductType.INAPP)
+                                                    .build()))
+                                    .build();
+
+                    billingClient.queryProductDetailsAsync(
+                            queryProductDetailsParams,
+                            new ProductDetailsResponseListener() {
+                                public void onProductDetailsResponse(@NonNull BillingResult billingResult,
+                                                                     @NonNull List<ProductDetails> productDetailsList) {
+                                    // check billingResult
+                                    // process returned productDetailsList
+                                    if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK){
+                                        for (ProductDetails productDetail : productDetailsList)
+                                            mProductDetails.put(productDetail.getProductId(), productDetail);
+                                    }
+                                }
+                            }
+                    );
+                }
+            }
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        });
+    }
+
+    void initializeBillingClient(Context context, String productType){
+        purchasesUpdatedListener = new PurchasesUpdatedListener() {
+            @Override
+            public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> purchases) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && purchases != null) {
+                    for (Purchase purchase : purchases) {
+                        if (productType.equals(BillingClient.ProductType.INAPP))
+                            handlePurchaseConsumable(purchase);
+                        else handlePurchaseNonConsumable(purchase);
+                    }
+                } else if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.USER_CANCELED) {
+                    // Handle an error caused by a user cancelling the purchase flow.
+                } else {
+                    // Handle any other error codes.
+                }
+            }
+        };
+
+        billingClient = BillingClient.newBuilder(context)
+                .setListener(purchasesUpdatedListener)
+                .enablePendingPurchases()
+                .build();
+    }
+
+    public void setConsumeResponseListener(ConsumeResponseListener consumeResponseListener) {
+        this.consumeResponseListener = consumeResponseListener;
+    }
+
+    void handlePurchaseConsumable(Purchase purchase) {
+        // Purchase retrieved from BillingClient#queryPurchasesAsync or your PurchasesUpdatedListener.
+
+        // Verify the purchase.
+        // Ensure entitlement was not already granted for this purchaseToken.
+        // Grant entitlement to the user.
+
+        ConsumeParams consumeParams =
+                ConsumeParams.newBuilder()
+                        .setPurchaseToken(purchase.getPurchaseToken())
+                        .build();
+        if (consumeResponseListener == null)
+            consumeResponseListener = new ConsumeResponseListener() {
+                @Override
+                public void onConsumeResponse(@NonNull BillingResult billingResult, @NonNull String purchaseToken) {
+                    if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                        // Handle the success of the consume operation.
+                    }
+                }
+            };
+
+        billingClient.consumeAsync(consumeParams, consumeResponseListener);
+    }
+
+    void handlePurchaseNonConsumable(Purchase purchase) {
+        if (purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED) {
+            if (!purchase.isAcknowledged()) {
+                AcknowledgePurchaseParams acknowledgePurchaseParams =
+                        AcknowledgePurchaseParams.newBuilder()
+                                .setPurchaseToken(purchase.getPurchaseToken())
+                                .build();
+                billingClient.acknowledgePurchase(acknowledgePurchaseParams, acknowledgePurchaseResponseListener);
+            }
+        }
+    }
+
+    public void setPurchasesResponseListener(PurchasesResponseListener purchasesResponseListener) {
+        this.purchasesResponseListener = purchasesResponseListener;
+    }
+
+    // call this method onResume to handle cases when purchase is made but
+    // your app lost track or was unaware of purchases
+    public void justInCasePurchaseListener() {
+        if (purchaseHistoryResponseListener == null) {
+            this.purchasesResponseListener = new PurchasesResponseListener() {
+                public void onQueryPurchasesResponse(@NonNull BillingResult billingResult, @NonNull List purchases) {
+                    // check billingResult
+                    // process returned purchase list, e.g. display the plans user owns
+
+                }
+            };
+        }
+        billingClient.queryPurchasesAsync(
+                QueryPurchasesParams.newBuilder()
+                        .setProductType(BillingClient.ProductType.SUBS)
+                        .build(),
+                purchasesResponseListener
+        );
+    }
+
+    public void setPurchaseHistoryResponseListener(PurchaseHistoryResponseListener purchaseHistoryResponseListener) {
+        this.purchaseHistoryResponseListener = purchaseHistoryResponseListener;
+    }
+
+    // get purchase history
+    public void getPurchaseHistory() {
+        if (purchaseHistoryResponseListener == null) {
+            this.purchaseHistoryResponseListener = new PurchaseHistoryResponseListener() {
+                public void onPurchaseHistoryResponse(
+                        @NonNull BillingResult billingResult, List purchasesHistoryList) {
+                    // check billingResult
+                    // process returned purchase history list, e.g. display purchase history
+                }
+            };
+        }
+        billingClient.queryPurchaseHistoryAsync(
+                QueryPurchaseHistoryParams.newBuilder()
+                        .setProductType(BillingClient.ProductType.SUBS)
+                        .build(),
+                purchaseHistoryResponseListener
+        );
+
+
+    }
+}
